35fca17b2b1d5a5dd97cafc7196873f7aeec48d0
Your GitHub Username <your-email@example.com>
2023-12-17 14:31:27 +0200

    v

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+const path = require('path');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +12,13 @@ const Users = require('./routes/Users')
 const port  = process.env.PORT ;
 
 app.use("/" , Users)
+
+app.use(express.static( '../final-project-react/my-final-project/src/App.jsx'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,"final-project-react/my-final-project/src" , 'app.jsx'));
+}); 
+
 
 
 
