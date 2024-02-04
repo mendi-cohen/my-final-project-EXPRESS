d77338332bb5b68b19da9603fb6b34133f1f590c
Your GitHub Username <your-email@example.com>
2024-02-04 14:40:55 +0200

    h

--- config/DB.js
+++ config/DB.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
   host: process.env.HOST,
-  dialect: 'mysql',
-  // dialect: 'postgres',
+  // dialect: 'mysql',
+  dialect: 'postgres',
 });
 
 module.exports = sequelize;
