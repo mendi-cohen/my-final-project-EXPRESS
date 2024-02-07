bdc21e9e695b9463a74ea3a402cb284a0bf19b1c
Your GitHub Username <your-email@example.com>
2024-02-07 20:16:45 +0200

    ח

--- model/Articles.js
+++ model/Articles.js
@@ -18,7 +18,7 @@ const Article = sequelize.define('articles', {
     allowNull: false,
   },
   date: {
-    type: DataTypes.DATE,
+    type: DataTypes.DATEONLY,
     allowNull: false,
   },
   time: {
